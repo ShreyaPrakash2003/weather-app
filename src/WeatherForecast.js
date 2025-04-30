a0356a73910c19ae06d2c9a54b42e98c7f099a17
ShreyaPrakash2003 <shreyaprakash2003@gmail.com>
2025-04-30 19:19:14 +0530

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const apiKey = "6ba52c12f1be33e38cfc2d9efcfed96d";
 
